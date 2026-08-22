af09adaf3bd4fc5e2dc192c0685384c07520e58d
agendanutrijulianamoreira-droid <agendanutrijulianamoreira@gmail.com>
2026-08-22 15:02:36 -0300

    feat: exibe resumo de evolução na Home

--- app/patient/home/page.tsx
+++ app/patient/home/page.tsx
@@ -1,5 +1,11 @@
+import { PatientEvolutionSummary } from "@/components/patient/PatientEvolutionSummary"
 import { PatientHomeV2 } from "@/components/patient/PatientHomeV2"
 
 export default function PatientHomePage() {
-  return <PatientHomeV2 />
+  return (
+    <>
+      <PatientHomeV2 />
+      <PatientEvolutionSummary />
+    </>
+  )
 }
