77d3589de9405bd3d96b2563e6fecbd214f39158
agendanutrijulianamoreira-droid <agendanutrijulianamoreira@gmail.com>
2026-08-22 15:14:20 -0300

    refactor: compartilha payload da home entre componentes

--- app/patient/home/page.tsx
+++ app/patient/home/page.tsx
@@ -1,13 +1,14 @@
 import { PatientEvolutionSummary } from "@/components/patient/PatientEvolutionSummary"
+import { PatientHomeDataProvider } from "@/components/patient/PatientHomeDataProvider"
 import { PatientHomeV2 } from "@/components/patient/PatientHomeV2"
 import { PatientRescueMode } from "@/components/patient/PatientRescueMode"
 
 export default function PatientHomePage() {
   return (
-    <>
+    <PatientHomeDataProvider>
       <PatientRescueMode />
       <PatientHomeV2 />
       <PatientEvolutionSummary />
-    </>
+    </PatientHomeDataProvider>
   )
 }
