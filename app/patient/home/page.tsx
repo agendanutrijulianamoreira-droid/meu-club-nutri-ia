c23b2d258526b9ae0ba51b7af60c963fb27c993d
agendanutrijulianamoreira-droid <agendanutrijulianamoreira@gmail.com>
2026-08-22 15:07:58 -0300

    feat: ativa modo resgate na Home

--- app/patient/home/page.tsx
+++ app/patient/home/page.tsx
@@ -1,9 +1,11 @@
 import { PatientEvolutionSummary } from "@/components/patient/PatientEvolutionSummary"
 import { PatientHomeV2 } from "@/components/patient/PatientHomeV2"
+import { PatientRescueMode } from "@/components/patient/PatientRescueMode"
 
 export default function PatientHomePage() {
   return (
     <>
+      <PatientRescueMode />
       <PatientHomeV2 />
       <PatientEvolutionSummary />
     </>
